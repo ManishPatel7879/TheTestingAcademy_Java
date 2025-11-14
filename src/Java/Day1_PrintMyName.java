6dddcb3587a2faeb0db527e7e12cf36ac8661bf5
Manish <manish7879patel@gmail.com>
2025-11-14 11:09:45 +0530

    New File

--- src/Java/Day1_PrintMyName.java
+++ src/Java/Day1_PrintMyName.java
@@ -1,6 +1,16 @@
 package Java;
 
-public class Day1_PrintMyName {
-    public static void main(String args[]) {
-    System.out.println("Ram");
-}}
+//public class Day1_PrintMyName {
+//    public static void main(String args[]) {
+//    System.out.println("Ram");
+//}}
+
+
+public class Day1_PrintMyName{
+    static void main(String[] args) {
+        System.out.println("Shortcut");
+    }
+
+
+
+}
---
+++ src/Java/Day2_Variables.java
@@ -0,0 +1,25 @@
+package Java;
+
+public class Day2_Variables {
+
+    public static void main() {
+
+        // below are local variables
+
+        int a = 10;
+        byte b = 10;
+        short c = 10;
+        long l = 9987987987987l;
+        float f =3.14f;
+        double d =    3453.3;
+        boolean b1 =  true;
+        char C = 'm';
+
+
+        System.out.println("Number is :" +a);
+
+
+    }
+
+
+}
